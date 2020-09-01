60c450570331f80634b19a3f8d3b62f0113e4a95
Brannon Fay <brannon.j.fay@gmail.com>
2020-09-01 19:19:28 -0400

    Attempting to import 3rd party Posh-SSH module. Fixed getting wsl address. Added verbose and standard output messaging Get windows host IP dynamically. ### Work to be done on this.  Not working at the moment. ###

--- wsl2/wsl2_setup_tcp_port_forwarding.ps1
+++ wsl2/wsl2_setup_tcp_port_forwarding.ps1
@@ -1,7 +1,11 @@
+Import-Module Posh-SSH
+
 #$remote_addr = bash.exe -c "ifconfig eth0 | grep 'inet '"
-$remote_addr = bash.exe -c "ip addr | grep -Ee 'inet.*eth0'"
+$remote_addr = bash.exe -c "ip addr show dev eth0 | grep 'inet '"
+Write-Verbose "Found IP information on WSL Host [$remote_addr]"
 #$remote_ip = "172.30.216.220"
 $found = $remote_addr -match '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}';
+Write-Verbose "IP Found from remote hose is [$found]"
 
 if( $found ){
   $remote_addr = $matches[0];
@@ -12,29 +16,37 @@ if( $found ){
 
 #[Ports]
 
-#All the ports you want to forward separated by coma
+#All the ports you want to forward separated by comma
 $ports=@(80,443,10000,3000,5000);
-
+Write-Verbose "Ports to be opened are [$ports]"
 
 #[Static ip]
 #You can change the addr to your ip config to listen to a specific address
-$addr='192.168.1.24';
+#$addr='192.168.1.24';
+$addr=(Get-NetIPAddress -AddressFamily IPv4 -PrefixOrigin DHCP | Select-Object IPAddressToString)[0].IPAddressToString
+Write-Verbose "Windows Host Address is [$addr]"
+
 $ports_a = $ports -join ",";
+Write-Verbose "Ports to be opened as csv list is [$ports_a]"
 
-
-#Remove Firewall Exception Rules
+Write-Output "Remove Firewall Exception Rules"
 Remove-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock'
 
-#adding Exception Rules for inbound and outbound Rules
+Write-Output "Add Exception Rules for inbound and outbound" 
 New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Outbound -LocalPort $ports_a -Action Allow -Protocol TCP;
 New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Inbound -LocalPort $ports_a -Action Allow -Protocol TCP;
 
 for( $i = 0; $i -lt $ports.length; $i++ ){
+  Write-Output "Removing Proxy for Port [$port]."
   $port = $ports[$i];
   #delete v4tov4 listenport=$port listenaddress=$addr
   Stop-SSHPortForward -BoundPort $port -BoundHost $addr
   #Invoke-Expression "netsh interface portproxy delete v4tov4 listenport=$port listenaddress=$addr";
   
+  Write-Output "Adding new proxy rule from [$addr] to [$remote_ip] for port [$port]."
   New-SSHRemotePortForward -LocalAdress $addr -LocalPort $port -RemoteAddress $remote_ip -RemotePort $port
   #Invoke-Expression "netsh interface portproxy add v4tov4 listenport=$port listenaddress=$addr connectport=$port connectaddress=$remoteport";
+  Write-Output "End of port loop."
 }
+
+Write-Output "Out of Port Proxy Loop. Fini."
